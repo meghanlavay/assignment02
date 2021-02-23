175970d8a146a2da65604797a09f1afa5bc7a1b1
meghanlavay <77410138+meghanlavay@users.noreply.github.com>
2021-02-23 15:54:05 -0800

    update

--- js/03-coinflip.js
+++ js/03-coinflip.js
@@ -1,17 +1,21 @@
 let randomNum = Math.round(Math.random());
 let coinFlip = randomNum;
-let choice = prompt('Select heads or tails');
-if (coinFlip < ".5") {
-    let result = 'heads';
-} else if (result = choice) {
-    if (result == 'heads') {
-    alert('The flip was heads and you chose heads...you win!');
+let choice = prompt(`Select heads or tails`);
+let flip;
+
+if (coinFlip === 1) {
+    flip = 'heads';
+    if (flip === choice) {
+        alert(`The flip was heads and you chose heads...you win!`);
     } else {
-    alert('The flip was heads and you chose tails...you lose!');
-    } else if {
-    if (result =='tails') {
-    alert('The flip was tails and you chose heads...you lose!');
+        alert(`The flip was heads and you chose tails...you lose!`);
+    }      
+} else {
+    flip = 'tails';
+    if (flip === choice) {
+        alert(`The flip was tails and you chose tails...you win!`);
     } else {
-    alert('The flip was tails and you chose tails...you win!');
+        alert(`The flip was tails and you chose heads...you lose!`);
+    }
 }
 
